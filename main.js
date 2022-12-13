73c9dacd9bf96eb5912754ba00e3053c4c656a68
Mogappu <sudarshana.mogappu@wipfli.com>
2022-12-13 19:44:29 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+console.log(1);
 $("#uploadButton").click(function(e){
-	display();
-})
+	e.preventDefault();
+	value = $("#fileUpload").val();
+	console.log("main",value);
+	var ext = value.substring(value.lastIndexOf('.') + 1);
+        if(ext == "pdf" )
+        {
+		var displayElement = document.querySelector("#displayPdf");
+	console.log("display",value);
+	console.log(3);
+	$('.accordion').html('<iframe src="value" frameborder="0" scrolling="no" id="myFrame"></iframe>');
+			console.log(2);
+        } 
+        else
+        {
+			alert("Upload a pdf file");
+        }
+	
+	
+
+});
+
