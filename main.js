25357bb3cd2f3fa09ba65c00146b8e3e0f32081f
Mogappu <sudarshana.mogappu@wipfli.com>
2022-12-21 18:48:11 +0530

    update

--- main.js
+++ main.js
@@ -11,20 +11,12 @@ dragArea.addEventListener('drop', (e) => {
 	e.preventDefault();
 	$('body').addClass('start-scrolling');
 	pdfFile = e.dataTransfer.files[0];
-
-	console.log("mainnnnnnnnnnnnnn", pdfFile);
-
 	if (pdfFile.type == 'application/pdf') {
-
 		const reader = new FileReader();
-
 		reader.readAsDataURL(pdfFile);
-
 		reader.onload = function (e) {
 			var pdf = reader.result;
 			$("#a").html(`<iframe src="${pdf}" style="height:600px;width:800px;" title="Iframe Example"></iframe>`);
-
-			console.log("main2", pdf);
 		}
 		$("#uploadButton").val("View Another");
 	}
@@ -33,41 +25,26 @@ dragArea.addEventListener('drop', (e) => {
 	}
 });
 
-
 $("#uploadButton").click(function (e) {
 	e.preventDefault();
-
 	$('body').addClass('start-scrolling');
-
 	var target = $('#fileUpload').get(0);
-
 	if (!target.files.length) {
 		return;
 	}
 	file = target.files[0];
-
 	if (file.type == 'application/pdf') {
-
-		console.log("main", file);
-
 		const reader = new FileReader();
-
-		console.log("main1", reader);
-
 		reader.readAsDataURL(file);
-
 		reader.onload = function (e) {
 			var pdf = reader.result;
 			$("#a").html(`<iframe src="${pdf}" style="height:600px;width:800px;" title="Iframe Example"></iframe>`);
-
-			console.log("main2", pdf);
 		}
 		$("#uploadButton").val("View Another");
 	}
 	else {
 		alert("Enter a pdf file");
 	}
-
 });
 
 
