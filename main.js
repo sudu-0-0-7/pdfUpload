3c1a670428a429d19c768a99c982f827fb70bc65
Mogappu <sudarshana.mogappu@wipfli.com>
2022-12-21 18:45:06 +0530

    update

--- main.js
+++ main.js
@@ -1,82 +1,73 @@
+$('body').addClass('stop-scrolling');
+const dragArea = document.querySelector('.dragAreaa');
+let pdfFile;
+
+dragArea.addEventListener('dragover', (e) => {
+	e.preventDefault();
+	console.log("File is inside the drag area");
+});
+
+dragArea.addEventListener('drop', (e) => {
+	e.preventDefault();
+	$('body').addClass('start-scrolling');
+	pdfFile = e.dataTransfer.files[0];
+
+	console.log("mainnnnnnnnnnnnnn", pdfFile);
+
+	if (pdfFile.type == 'application/pdf') {
+
+		const reader = new FileReader();
+
+		reader.readAsDataURL(pdfFile);
+
+		reader.onload = function (e) {
+			var pdf = reader.result;
+			$("#a").html(`<iframe src="${pdf}" style="height:600px;width:800px;" title="Iframe Example"></iframe>`);
+
+			console.log("main2", pdf);
+		}
+		$("#uploadButton").val("View Another");
+	}
+	else {
+		alert("Enter a pdf file");
+	}
+});
+
 
 $("#uploadButton").click(function (e) {
 	e.preventDefault();
 
+	$('body').addClass('start-scrolling');
+
 	var target = $('#fileUpload').get(0);
 
-	//console.log(target.val);
 	if (!target.files.length) {
 		return;
 	}
 	file = target.files[0];
 
-	console.log("main", file);
+	if (file.type == 'application/pdf') {
 
-	const reader = new FileReader();
+		console.log("main", file);
 
-	console.log("main1", reader);
+		const reader = new FileReader();
 
-	reader.readAsArrayBuffer(file);
+		console.log("main1", reader);
 
-	//reader.readAsText(file);
+		reader.readAsDataURL(file);
 
+		reader.onload = function (e) {
+			var pdf = reader.result;
+			$("#a").html(`<iframe src="${pdf}" style="height:600px;width:800px;" title="Iframe Example"></iframe>`);
 
-
-	
-
-	//reader.readAsText(file);
-
-	reader.onload = function(e){
-		var arrayBuffer = reader.result;
-
-		console.log("main2",arrayBuffer);
-		//alert(arrayBuffer);
-
-		//console.log("SSSS",arrayBuffer.Uint8Array);
+			console.log("main2", pdf);
+		}
+		$("#uploadButton").val("View Another");
 	}
-	
-	
-
-	
-	//const array = window.array.readAsArrayBuffer(file);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// var target = $('#fileUpload').get(0);
-	// if (!target.files.length) {
-	// 	return;
-	// }
-	// file = target.files[0];
-
-	// console.log("main", file);
-	// if (file.type !=="application/pdf") {
-	// 	alert("Upload a pdf file");
-	// 	return;
-	// }
-	// const objectURL = window.URL.createObjectURL(file);
-
-	// //
-
-
-	$("#a").html(`<iframe src="${arrayBuffer}" style="height:600px;width:800px;" title="Iframe Example"></iframe>`);
-
-	$("#uploadButton").val("View Another");
+	else {
+		alert("Enter a pdf file");
+	}
 
 });
+
 
